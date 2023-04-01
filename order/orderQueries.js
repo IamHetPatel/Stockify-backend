63730a3cbfbbd77370951ce65a1f649eb581ed7c
Het Patel <hetp943@gmail.com>
2023-04-02 01:38:27 +0530

    create order will only need, product & supplier names, not ID

--- order/orderQueries.js
+++ order/orderQueries.js
@@ -1,30 +1,30 @@
 const db = require("../db/conn");
 
-exports.createOrder = (data,id, callback) => {
-  let ts = Date.now();
-let date_ob = new Date(ts);
-  let date = date_ob.getDate();
-  let month = date_ob.getMonth() + 1; 
-  let year = date_ob.getFullYear();
-  let today = year + "-" + month + "-" + date 
-  db.query(
-    `INSERT INTO ORDERS (ORDER_ID, PRODUCT_ID, SUPPLIER_ID, DATE, QUANTITY, USER_ID) values (?,?,?,?,?,?)`,
-    [
-      data.ORDER_ID,
-      data.PRODUCT_ID,
-      data.SUPPLIER_ID,
-      today,
-      data.QUANTITY,
-      id
-    ],
-    (error, results, fields) => {
-      if (error) {
-        return callback(error);
-      }
-      return callback(null, results);
-    }
-  );
-};
+// exports.createOrder = (data,id, callback) => {
+//   let ts = Date.now();
+// let date_ob = new Date(ts);
+//   let date = date_ob.getDate();
+//   let month = date_ob.getMonth() + 1; 
+//   let year = date_ob.getFullYear();
+//   let today = year + "-" + month + "-" + date 
+//   db.query(
+//     `INSERT INTO ORDERS (ORDER_ID, PRODUCT_ID, SUPPLIER_ID, DATE, QUANTITY, USER_ID) values (?,?,?,?,?,?)`,
+//     [
+//       data.ORDER_ID,
+//       data.PRODUCT_ID,
+//       data.SUPPLIER_ID,
+//       today,
+//       data.QUANTITY,
+//       id
+//     ],
+//     (error, results, fields) => {
+//       if (error) {
+//         return callback(error);
+//       }
+//       return callback(null, results);
+//     }
+//   );
+// };
 
 exports.getOrders = (id,callback) => {
     db.query(`SELECT O.ORDER_ID, P.PRODUCT_NAME, S.NAME,O.DATE, O.QUANTITY 
--- order/orderRoutes.js
+++ order/orderRoutes.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 const orderController = require("./orderController");
 
+
 const jwt_validation = require("./../auth/jwt_validation");
 
+// router.post("/", jwt_validation.checkJwt, orderController.addOrder);
+router.get("/", jwt_validation.checkJwt, orderController.getOrdersList);
 router.post("/", jwt_validation.checkJwt, orderController.addOrder);
-router.get("/",jwt_validation.checkJwt, orderController.getOrdersList);
+
 module.exports = router;
