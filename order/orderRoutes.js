550825b576e485d5f13407ba7e3444a4390f4c54
Het Patel <hetp943@gmail.com>
2023-04-02 23:19:19 +0530

    order changes

--- order/orderRoutes.js
+++ order/orderRoutes.js
@@ -7,6 +7,6 @@ const jwt_validation = require("./../auth/jwt_validation");
 
 // router.post("/", jwt_validation.checkJwt, orderController.addOrder);
 router.get("/", jwt_validation.checkJwt, orderController.getOrdersList);
-router.post("/", orderController.addOrder);
+router.post("/", jwt_validation.checkJwt, orderController.addOrder);
 
 module.exports = router;
