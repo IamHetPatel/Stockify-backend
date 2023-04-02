133b8f690f25a82bffb182c1718b75bd3b946c6f
Het Patel <hetp943@gmail.com>
2023-04-02 23:28:53 +0530

    order changes

--- order/orderController.js
+++ order/orderController.js
@@ -7,16 +7,15 @@ exports.addOrder = (req, res) => {
 
   const productName = req.body.PRODUCT_NAME;
   const supplierName = req.body.SUPPLIER_NAME;
-
   const sql1 = "SELECT product_id FROM PRODUCT WHERE PRODUCT_NAME = ?";
   const sql2 = "SELECT SUPPLIER_ID FROM SUPPLIER WHERE NAME = ?";
-  
+
   db.query(sql1, [productName], (Proderr, Prodresult) => {
     if (Proderr) {
       console.log(Proderr);
       res.status(500).send("Error searching for product ID.");
     } else {
-      const productId = Prodresult[0].product_id;
+      const productId = Prodresult[0].PRODUCT_ID;
 
       db.query(sql2, [supplierName], (Supperr, Suppresult) => {
         if (Supperr) {
