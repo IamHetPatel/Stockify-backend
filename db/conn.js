399db640c36e25feae453e3731f3eef95fcccd86
Het Patel <hetp943@gmail.com>
2023-04-09 11:53:06 +0530

    error starting server

--- db/conn.js
+++ db/conn.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 require('dotenv').config({path: __dirname + '../.env'})
 const db = mysql.createConnection({
-  host: process.env["DB_host"],
-  user: process.env["DB_user"],
-  password: process.env["DB_password"],
-  database: process.env["DB"],
+  host: "db4free.net",
+  user: "inventory",
+  password: "nihal123",
+  database: "inventory_manage",
 });
 
 module.exports = db;
